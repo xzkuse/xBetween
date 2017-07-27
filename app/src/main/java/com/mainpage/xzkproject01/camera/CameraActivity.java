fa386cd9120757833a7c0ab585e37abe399a7044
xzk <544026777@qq.com>
2017-07-27 18:02:57 +0800

    Signed-off-by: xzk <544026777@qq.com>

--- app/src/main/java/com/mainpage/xzkproject01/camera/CameraActivity.java
+++ app/src/main/java/com/mainpage/xzkproject01/camera/CameraActivity.java
@@ -1,10 +1,16 @@
 package com.mainpage.xzkproject01.camera;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 
 import com.mainpage.xzkproject01.R;
+
+import java.util.Timer;
 
 /**
  * create: 2017/7/26 0026
@@ -18,5 +24,15 @@ public class CameraActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(R.layout.activity_camera);
+        builder.show();
+    }
+
+    private void iniStoggle() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
